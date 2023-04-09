0f4e0b556e948e96985583bae91eb112a2e5a4a1
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-04-09 14:34:10 +0530

    Scheduled Job for rewriting seats data everyday at 6AM IST

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import router from './routes/train.js';
 import cors from 'cors';
+import cron from 'node-cron';
+import { spawn } from 'child_process';
 
 dotenv.config();
 
@@ -30,6 +32,24 @@ mongoose
 
 // Route for handling train data and bookings
 app.use('/api/train', router);
+
+// Schedule task to run seed.js file every day at 6AM IST
+cron.schedule('0 6 * * *', () => {
+  console.log('Running seed.js file');
+  const seed = spawn('node', ['seed.js']);
+
+  seed.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  seed.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  seed.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
